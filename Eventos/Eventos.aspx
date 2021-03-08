145af119ffe8ee2b3ca74d2106a98db2ef97f810
abraham <abrahamaltuve@unisinu.edu.co>
2021-03-07 22:22:40 -0500

    Agregar archivos de proyecto.

---
+++ Eventos/Eventos.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Eventos.aspx.cs" Inherits="Eventos.Eventos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridViewEventos" runat="server">
+                <Columns>
+                    <asp:BoundField DataField="eventos" HeaderText="Eventos" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Eventos/Eventos.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eventos
+{
+    public partial class Eventos : System.Web.UI.Page
+    {
+        private string[] eventos = new string[9];
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Response.Write("PreInit<br/>");
+            eventos.Append("PreInit");
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("Init<br/>");
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            Response.Write("Init Complete<br/>");
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            Response.Write("PreLoad<br/>");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Load<br/>");
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            Response.Write("Load Complete<br/>");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Response.Write("PreRender <br/>");
+        }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            Response.Write("PreRender Complete <br/>");
+            DataTable dt = new DataTable();
+            GridViewEventos.DataSource = dt;
+            GridViewEventos.DataBind();
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            //Response.Write("Unload");
+        }
+
+
+
+    }
+}
